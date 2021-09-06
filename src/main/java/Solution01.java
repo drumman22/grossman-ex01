f6e90d65ca1a22edc699ead61f8c956c9067e903
drumman22 <grossmanjason0@gmail.com>
2021-09-05 20:10:13 -0400

    Initial commit

---
+++ src/main/java/Solution01.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Jason Grossman
+ */
+
+import java.util.Scanner;
+
+public class Solution01 {
+    public static void main(String[] args) {
+        // Setuper scanner and get input
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = input.nextLine();
+
+        // Output name
+        System.out.printf("Hello, %s, nice to meet you!%n", name);
+    }
+}
